a6df121a642498921492d7d8e9576789d3b36f47
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-03-23 23:12:41 +0000

    "changed the style of the title and inserted the radio buttons on the landing page"

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { Button, Input } from '@chakra-ui/react';
+import { Button, Input, ModalHeader } from '@chakra-ui/react';
 import React from 'react'
 import './home.css';
 
@@ -6,17 +6,30 @@ const Home = () => {
   return (
     <div className='container'>
       <div className='header'>
-        Jobs Hub
+        <h1>Jobs Hub</h1>
       </div>
       <div className='banner'>
-          <Input />
+          <Input  mt='90px' w='500px' placeholder='Title, companies, expertise or benefits'/>
           <Button>Search</Button>
       </div>
       <div className='content'>
         <div className='contentSideNavbar'>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
+            <div >
+                <input type='checkbox' />
+                Full time
+            </div>
+            <div>
+                <div>Location</div>
+                <Input />
+            </div>
+            <div className='location'>
+                <div><input type='radio' />
+                state of the company</div>
+                <div><input type='radio' />
+                state of the company</div>
+                <div><input type='radio' />
+                state of the company</div>
+            </div>
         </div>
         <div className='contentJobsList'>
             list
