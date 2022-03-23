e8982298054eddd421153232b74da4d1d28ae4cc
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-03-23 06:49:28 +0000

    "First layout for the landing page"

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './home.css';
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <div className='header'>
+          Jobs Hub
+      </div>
+      <div className='banner'>
+          background image
+      </div>
+      <div className='content'>
+        <div className='contentSideNavbar'>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+        </div>
+        <div className='contentJobsList'>
+            list
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
