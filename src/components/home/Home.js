6a176e1f063cd3de98fdbd7d18872a29c6eaa995
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-03-29 15:05:18 +0000

    "This is display the sidebar and job list"

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,42 +1,18 @@
-import { Button, Input, ModalHeader } from '@chakra-ui/react';
 import React from 'react'
-import './home.css';
+import './home.css'
+import Sidebar from './sidebar/Sidebar'
+import JobContainer from './jobsContainer/JobContainer'
+import { Box } from '@chakra-ui/react'
 
 const Home = () => {
   return (
-    <div className='container'>
-      <div className='header'>
-        <h1>Jobs Hub</h1>
-      </div>
-      <div className='banner'>
-          <Input  mt='90px' w='500px' placeholder='Title, companies, expertise or benefits'/>
-          <Button>Search</Button>
-      </div>
-      <div className='content'>
-        <div className='contentSideNavbar'>
-            <div >
-                <input type='checkbox' />
-                Full time
-            </div>
-            <div>
-                <div>Location</div>
-                <Input />
-            </div>
-            <div className='location'>
-                <div><input type='radio' />
-                state of the company</div>
-                <div><input type='radio' />
-                state of the company</div>
-                <div><input type='radio' />
-                state of the company</div>
-            </div>
-        </div>
-        <div className='contentJobsList'>
-            list
-        </div>
-      </div>
-    </div>
+    <Box className='home'>
+      <Sidebar />
+      <JobContainer />
+    </Box>
   )
 }
 
 export default Home
+
+
