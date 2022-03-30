2eee88cebc64a033c112667c9434c4726aea6527
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-03-30 09:17:08 +0000

    "Used the useEffect to fetch jobs and display "

--- src/components/home/jobsContainer/JobContainer.js
+++ src/components/home/jobsContainer/JobContainer.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './JobContainer.css'
 // import axios from "axios";
-import { Box, Button, Image, Text } from '@chakra-ui/react'
+import { Box, Button, Image } from '@chakra-ui/react'
+
 
 const JobContainer = () => {
 
@@ -11,15 +12,55 @@ const JobContainer = () => {
   const fetchJobs = async () => {
     const response = await fetch("https://remotive.io/api/remote-jobs")
     const jsonResponse = await response.json()
-    setJobs(jsonResponse);
-    console.log(jobs)
+    setJobs(jsonResponse.jobs);
+    console.log(jsonResponse)
+    console.log(jsonResponse['jobs'])
   }
-  fetchJobs()
+
+  useEffect(() => {fetchJobs()}, [])
+  
 
   return (
-    <div>
+    <Box classsName="Container">
+      {jobs.map((job) => {
+
+      return  <Box display='flex' className='job_display'>
+                <Image src={job.company_logo} alt='Company_logo' borderRadius='10' m='10'/>
+                <Box className='job_name'>
+                  <h2>{job.company_name}</h2>
+                  <h1>{job.title}</h1>
+                  <Button>{job.job_type}</Button>
+                </Box>
+                <h6>{job.candidate_required_location}</h6>
+                <h6>{job.publication_date}</h6>
+              </Box>
+      })}
+    </Box>
+    // <Box className='Container'>
+    //   {/* <Box display='flex' className='job_display'>
+    //     <Image src={Fakelogo} alt='Company_logo' borderRadius='10' m='10'/>
+    //     <Box className='job_name'>
+    //       <h2>Kasisto</h2>
+    //       <h1>Front-End Software Engineer</h1>
+    //       <Button>Full time</Button>
+    //     </Box>
+    //     <h6>public logo + the state</h6>
+    //     <h6>clock logo + days </h6>
+    //   </Box> */}
       
-    </div>
+      
+      
+    //   {/* <Box display='flex' className='job_display'>
+    //     <Image src={Fakelogo} alt='Company_logo' borderRadius='10' m='10'/>
+    //     <Box className='job_name'>
+    //       <span>Kasisto</span>
+    //       <h1>Front-End Software Engineer</h1>
+    //       <Button>Full time</Button>
+    //     </Box>
+    //     <h6>public logo + the state</h6>
+    //     <h6>clock logo + days </h6>
+    //   </Box> */}
+    // </Box>
   )
 }
 
