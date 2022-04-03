ef174ba48ef08d27484a1b3f0356c9b83351f6a2
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-04-03 18:18:13 +0000

    "This loading file loads when waiting search or checkout"

---
+++ src/components/home/Loading.js
@@ -0,0 +1,12 @@
+import spinner from './spinner.gif';
+import './loading.css'
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <img src={spinner} alt="Loading" className='loading-img' />
+    </div>
+  );
+};
+
+export default Loading;
