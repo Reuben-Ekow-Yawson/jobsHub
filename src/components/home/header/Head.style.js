9e39df2b9ebf31a25fb7136f056bd8b610202df1
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-04-05 00:19:59 +0000

    "Style component for Header"

---
+++ src/components/home/header/Head.style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyleHeader = styled.div`
+
+    padding: 32px 0;
+
+  .title {
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 36px;
+    font-family: "Poppins", sans-serif;
+    color: #282538;
+    
+    span {
+      font-weight: 400;
+    }
+  }
+
+`
