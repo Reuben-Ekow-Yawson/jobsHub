d6fc1b399cab346093fc586654f8124771b6dd62
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-04-05 00:13:07 +0000

    "Style component for Footer"

---
+++ src/components/home/footer/Foot.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FooterStyle = styled.div`
+  padding: 40px 0;
+  text-align: center;
+  background-color: #f6f7fb;
+
+  a {
+    color: inherit;
+    text-decoration: underline;
+    text-underline-offset: 1px;
+    font-weight: 700;
+  }
+`
