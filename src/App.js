daa4006ed2a4de3f3839712d372aec688d811143
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-04-05 00:01:17 +0000

    "Did nothing"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Footer from './components/home/footer/Footer';
-import Header from './components/home/header/Header';
+import Footer from "./components/home/footer/Footer";
+import Header from "./components/home/header/Header";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Search from "./components/home/searchHeader/Search";
@@ -8,42 +8,39 @@ import JobDescription from "./components/home/jobDetail/JobDescription";
 import JobFilter from "./components/home/sidebar/JobFilter";
 
 import { JobContextProvider } from "./context/JobContext";
-import './App.css'
-
+import "./App.css";
 
 function App() {
-  
   return (
     <JobContextProvider>
-    <Router>
-      {/* HEADER */}
-      <Header />
+      <Router>
+        {/* HEADER */}
+        <Header />
 
-      {/* SEARCH FORM */}
-      <Search />
+        {/* SEARCH FORM */}
+        <Search />
 
-      <main>
-        <div className="container">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <JobFilter />
-                  <JobList />
-                </>
-              }
-            />
-            <Route path="/:jobDetailUrl" element={<JobDescription />} />
-          </Routes>
-        </div>
-      </main>
+        <main>
+          <div className="container">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <JobFilter />
+                    <JobList />
+                  </>
+                }
+              />
+              <Route path="/:jobDetailUrl" element={<JobDescription />} />
+            </Routes>
+          </div>
+        </main>
 
-      {/* FOOTER */}
-      <Footer />
-    </Router>
-  </JobContextProvider>
-
+        {/* FOOTER */}
+        <Footer />
+      </Router>
+    </JobContextProvider>
   );
 }
 
