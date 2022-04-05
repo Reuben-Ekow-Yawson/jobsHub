532c15bfd687bba7d115ad9eb87054346bb4dc11
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-04-05 00:04:49 +0000

    "Style component for the loading"

---
+++ src/components/home/StyleLoad.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Load = styled.div`
+  min-height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+
+img {
+    max-width: 20px;
+  }
+`
